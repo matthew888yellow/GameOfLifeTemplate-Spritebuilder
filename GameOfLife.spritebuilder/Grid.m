29b2aced6bf9d275eccb0a61dc763524e9df8770
matthew888yellow <Matthew@Matthews-MacBook-Pro.local>
2015-06-04 18:17:36 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -149,15 +149,13 @@ static const int GRID_COLUMNS = 10;
             // access the creature in the cell that corresponds to the current row/column
             Creature *currentCreature = _gridArray[i][j];
             
-            if(currentCreature.livingNeighbors==3)
-            {
-                currentCreature.isAlive=TRUE;
-                numAlive=numAlive+1;
-            }
+            if(currentCreature.livingNeighbors==3) numAlive=numAlive+1;
             else
             {
                 if(currentCreature.livingNeighbors<=1 || currentCreature.livingNeighbors>=4) currentCreature.isAlive=FALSE;
             }
+            
+            if(currentCreature.isAlive) numAlive++;
         }
     }
     _totalAlive = numAlive;
