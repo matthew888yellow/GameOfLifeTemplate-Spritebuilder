717f52a4842959db3875b0e0c9de60d215050af2
matthew888yellow <Matthew@Matthews-MacBook-Pro.local>
2015-06-04 18:00:41 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.h
+++ GameOfLife.spritebuilder/Grid.h
@@ -14,7 +14,7 @@
 @property (nonatomic, assign) int generation;
 
 - (void)evolveStep;
-- (BOOL)isIndexValidForX:(int)x andY:(int)y
+- (BOOL)isIndexValidForX:(int)x andY:(int)y;
 - (void)countNeighbors;
 - (void)updateCreatures;
 
